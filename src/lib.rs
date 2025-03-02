2faf00a448bfc702006bd21e021b22eada847077
Philippe LE <le.philippe.duc.tai@gmail.com>
2025-03-02 08:38:32 +0100

    Correction.

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,7 @@ pub fn time_it(_attr: TokenStream, item: TokenStream) -> TokenStream {
     TokenStream::from(quote! {
         #vis #sig {
             use tracing::{info};
+            use tracing_subscriber;
             tracing_subscriber::fmt::init();
             let start = std::time::Instant::now();
             let result = { #block };
